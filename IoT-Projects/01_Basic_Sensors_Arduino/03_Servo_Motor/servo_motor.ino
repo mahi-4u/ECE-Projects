04ceb70e16a7875c71cdd3240b9406dfc09deaca
Mahi's-electronics <ismahi4u@gmail.com>
2026-02-28 11:34:42 +0530

    Update servo_motor.ino

--- IoT-Projects/01_Basic_Sensors_Arduino/03_Servo_Motor/servo_motor.ino
+++ IoT-Projects/01_Basic_Sensors_Arduino/03_Servo_Motor/servo_motor.ino
@@ -1,14 +1,18 @@
-#include <Servo.h>       // Include library to control servo
+// Servo Motor Control using Arduino Uno
+// Author: Mahesh
+// Servo connected to Digital Pin 9
 
-Servo myServo;           // Create servo object
+#include <Servo.h>
+
+Servo myServo;
 
 void setup() {
-  myServo.attach(9);     // Connect servo to pin 9
+  myServo.attach(9);   // Attach servo to pin 9
 }
 
 void loop() {
   for (int angle = 0; angle <= 180; angle += 45) {
-    myServo.write(angle);  // Rotate to 0, 45, 90, 135, 180 degrees
-    delay(1000);           // Wait 1 second between movements
+    myServo.write(angle);   // Rotate to specific angle
+    delay(1000);            // Wait 1 second
   }
 }
