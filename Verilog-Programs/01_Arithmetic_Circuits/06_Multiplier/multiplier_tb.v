47c4244cf1da15fd5f2ac0ae5fd88b8abbfa54e2
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 01:13:39 +0530

    Update multiplier_tb.v

--- Verilog-Programs/01_Arithmetic_Circuits/06_Multiplier/multiplier_tb.v
+++ Verilog-Programs/01_Arithmetic_Circuits/06_Multiplier/multiplier_tb.v
@@ -1,1 +1,28 @@
+module multipliers_tb(
 
+    );
+    // Test bench for 2-bit multiplication
+reg [1:0]a;
+reg [1:0]b;
+wire [3:0]product;
+multipliers uut(
+.a(a),
+.b(b),
+.product(product)
+);
+// clock generation
+//stimulus
+initial begin
+a= 2'b10;
+b= 2'b01;
+#10 a=2'b10;b=2'b10;
+//Apply stimulus
+#10 a= 2'b11; b= 2'b10;
+// End simulation
+
+end
+// Monitor
+always @(a or b)
+$display("$time=%0t: a=%b b=%b product=%b",$time,a,b,product);
+
+endmodule
