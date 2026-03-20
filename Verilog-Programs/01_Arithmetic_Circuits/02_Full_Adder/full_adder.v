a3f77f92d29cfe171fb583efb3a52ddbc324411a
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 00:32:17 +0530

    Update full_adder.v

--- Verilog-Programs/01_Arithmetic_Circuits/02_Full_Adder/full_adder.v
+++ Verilog-Programs/01_Arithmetic_Circuits/02_Full_Adder/full_adder.v
@@ -1,1 +1,21 @@
-
+module fulladd(
+    input a,
+    input b,
+    input cin,
+    output  s,
+    output  cout
+    );
+    wire a1,a2,r1;
+   //assign s=a^b^cin; //dataflow
+   //assign cout=(a&b)|(cin&(a|b));
+   /*always@(*) //behavioural
+   begin
+   s=a^b^cin;
+   cout=(a&b)|(cin&(a|b));
+   end*/
+   xor (s,a,b,cin); //gatelevel
+   and (a1,a,b);
+   or (r1,a,b);
+   and (a2,cin,r1);
+   or (cout,a1,a2);
+endmodule
