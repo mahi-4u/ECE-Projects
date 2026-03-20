a058608234354f3e0f3cf5334ee91cafe482617a
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 00:37:33 +0530

    Update full_adder_tb.v

--- Verilog-Programs/01_Arithmetic_Circuits/02_Full_Adder/full_adder_tb.v
+++ Verilog-Programs/01_Arithmetic_Circuits/02_Full_Adder/full_adder_tb.v
@@ -1,1 +1,15 @@
-
+module fulladd_tb(
+    );
+    reg a,b,cin;
+    wire s,cout;
+    fulladd uut(.a(a),.b(b),.cin(cin),.s(s),.cout(cout));
+    initial
+    begin
+    a=0;b=0;cin=0;
+    #100 a=0;b=1;cin=0;
+    #100 a=1;b=0;cin=0;
+    #100 a=1;b=0;cin=0;
+    #100 a=1;b=1;cin=0;
+    $display("a=%b,b=%b,cin=%b,s=%b,cout=%b",a,b,cin,s,cout);
+    end
+endmodule
