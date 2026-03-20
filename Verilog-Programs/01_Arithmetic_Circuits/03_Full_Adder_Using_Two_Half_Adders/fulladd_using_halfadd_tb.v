217a6ceecc08853f86d3c7fa2fae44047a109a9d
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 01:12:31 +0530

    Update fulladd_using_halfadd_tb.v

--- Verilog-Programs/01_Arithmetic_Circuits/03_Full_Adder_Using_Two_Half_Adders/fulladd_using_halfadd_tb.v
+++ Verilog-Programs/01_Arithmetic_Circuits/03_Full_Adder_Using_Two_Half_Adders/fulladd_using_halfadd_tb.v
@@ -1,1 +1,17 @@
+module full_halfadd_tb(
 
+    );
+    reg a,b,cin;
+    wire sum,cout;
+    full_half_add uut(.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout));
+    initial
+    begin
+    a=0;b=0;cin=0;
+    #100 a=0;b=1;cin=0;
+    #100 a=1;b=0;cin=0;
+    #100 a=1;b=0;cin=0;
+    #100 a=1;b=1;cin=0;
+    $display("a=%b,b=%b,cin=%b,sum=%b,,cout=%b",a,b,cin,sum,cout);
+    end
+
+endmodule
