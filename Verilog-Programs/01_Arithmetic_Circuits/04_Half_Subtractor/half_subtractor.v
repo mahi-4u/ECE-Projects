43b119be65035311fca4eb6ac4d908fc6e718517
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 00:50:41 +0530

    Update half_subtractor.v

--- Verilog-Programs/01_Arithmetic_Circuits/04_Half_Subtractor/half_subtractor.v
+++ Verilog-Programs/01_Arithmetic_Circuits/04_Half_Subtractor/half_subtractor.v
@@ -1,1 +1,20 @@
-
+//Author:mahesh
+//halfsub using three models of code
+module half_sub(
+    input a,
+    input b,
+    output d,
+    output bout
+    );
+    //wire abar;
+    assign d = a ^ b ;//dataflow
+    assign bout = ~a & b;
+    /*xor (d,a,b);//gatelevel
+    not(abar,a);
+    and (bout,abar,b);*/
+    /*always@(*)//behavioural
+    begin
+    d=a^b;
+    bout= ~a & b;
+    end*/
+endmodule
