975ab8197065cd947ca8e3e0bdd8b63db2c51b0c
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 01:01:15 +0530

    Update fulladd_using_halfadd.v

--- Verilog-Programs/01_Arithmetic_Circuits/03_Full_Adder_Using_Two_Half_Adders/fulladd_using_halfadd.v
+++ Verilog-Programs/01_Arithmetic_Circuits/03_Full_Adder_Using_Two_Half_Adders/fulladd_using_halfadd.v
@@ -1,1 +1,17 @@
-
+//Author:mahesh
+module full_half_add(
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output cout
+    );
+    wire x,y,z;
+    half ha1(.s(x),.c(y),.a(a),.b(b));
+    half ha2(.s(sum),.c(z),.a(x),.b(cin));
+    or (cout,y,z);
+endmodule
+module half(output s,output c,input a,input b);
+assign s=a^b;
+assign c=a&b;
+endmodule
