4dd69a9962218a2502e5a11ee367455ae6faf2a8
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 00:23:48 +0530

    Update half_adder_tb.v

--- Verilog-Programs/01_Arithmetic_Circuits/01_Half_Adder/half_adder_tb.v
+++ Verilog-Programs/01_Arithmetic_Circuits/01_Half_Adder/half_adder_tb.v
@@ -1,1 +1,14 @@
-
+module half_add_tb(
+    );
+    reg a,b;
+    wire s,c;
+    half_add uut(.a(a),.b(b),.s(s),.c(c));
+    initial
+    begin
+    a=0;b=0;
+    #100 a=0;b=1;
+    #100 a=1;b=0;
+    #100 a=1;b=1;
+    $display("a=%b,b=%b,s=%b,c=%b",a,b,s,c);
+    end
+endmodule
