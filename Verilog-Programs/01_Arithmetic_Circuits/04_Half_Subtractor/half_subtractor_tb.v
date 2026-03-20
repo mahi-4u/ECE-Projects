6925cd566d949c53d0e0632be2a54fe698c6f940
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 00:51:20 +0530

    Update half_subtractor_tb.v

--- Verilog-Programs/01_Arithmetic_Circuits/04_Half_Subtractor/half_subtractor_tb.v
+++ Verilog-Programs/01_Arithmetic_Circuits/04_Half_Subtractor/half_subtractor_tb.v
@@ -1,1 +1,13 @@
-
+module half_subtb(
+    );
+    reg a,b;
+    wire d,bout;
+    half_sub uut(.a(a),.b(b),.d(d),.bout(bout));
+    initial
+    begin
+    a=0;b=0;
+    #100 a=0;b=1;
+    #100 a=1;b=0;
+    #100 a=1;b=1;
+    end
+endmodule
