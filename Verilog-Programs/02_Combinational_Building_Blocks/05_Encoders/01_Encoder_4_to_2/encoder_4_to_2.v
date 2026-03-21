2df6fa8ed20ef857b9038cadf5681a51d28a373b
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 22:44:56 +0530

    Update encoder_4_to_2.v

--- Verilog-Programs/02_Combinational_Building_Blocks/05_Encoders/01_Encoder_4_to_2/encoder_4_to_2.v
+++ Verilog-Programs/02_Combinational_Building_Blocks/05_Encoders/01_Encoder_4_to_2/encoder_4_to_2.v
@@ -1,1 +1,25 @@
+//AUthor:mahesh
+//you can find three models of code
+module four_two_encoder(
+input a,b,c,d,
+output x,y
+    );
+    //GATE LEVEL MODELLING
+or(x,c,d);
+or(y,b,d);
 
+/*DATAFLOW MODELLING);
+assign x=c|d;
+assign y=b|d;*/
+
+/*Behavioural
+always @(*)
+begin
+case({a,b,c,d})
+4'b1000:begin x=0;y=0; end
+4'b0100:begin x=0;y=1; end
+4'b0010:begin x=1;y=0; end
+4'b0001:begin x=1;y=1; end
+endcase
+end*/
+endmodule
