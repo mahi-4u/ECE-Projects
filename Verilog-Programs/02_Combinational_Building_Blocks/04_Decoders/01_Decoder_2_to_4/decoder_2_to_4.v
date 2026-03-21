e4bb396eabe341f85ac1e1bf921cbcc22c96a03e
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 22:35:13 +0530

    Update decoder_2_to_4.v

--- Verilog-Programs/02_Combinational_Building_Blocks/04_Decoders/01_Decoder_2_to_4/decoder_2_to_4.v
+++ Verilog-Programs/02_Combinational_Building_Blocks/04_Decoders/01_Decoder_2_to_4/decoder_2_to_4.v
@@ -1,1 +1,30 @@
+//Author:mahesh
+//you can find three models of code here
+module two_four_decoder(
+input a0,a1,
+output [3:0]D);
+/*GATE LEVEL MODELLING
+wire an0,an1;
+not (an0,a0);
+not (an1,a1);
+and (D[0],an0,an1);
+and (D[1],an0,a1);
+and (D[2],a0,an1);
+and (D[3],a0,a1);*/
 
+//DATAFLOW MODELLING
+assign D[0]=(~a0)&(~a1);
+assign D[1]=(~a0)&a1;
+assign D[2]=a0&(~a1);
+assign D[3]=a0&a1;
+
+/*Behavioural MODELLING
+always@(*)
+begin
+D[0]=(~a0)&(~a1);
+D[1]=(~a0)&a1;
+D[2]=a0&(~a1);
+D[3]=a0&a1;
+end*/
+   
+endmodule
