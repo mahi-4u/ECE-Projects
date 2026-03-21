79730d55dda267bc8146ab7401d72b9c41ce76a8
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 22:36:06 +0530

    Update decoder_2_to_4_tb.v

--- Verilog-Programs/02_Combinational_Building_Blocks/04_Decoders/01_Decoder_2_to_4/decoder_2_to_4_tb.v
+++ Verilog-Programs/02_Combinational_Building_Blocks/04_Decoders/01_Decoder_2_to_4/decoder_2_to_4_tb.v
@@ -1,1 +1,13 @@
-
+module two_four_decodertb(
+    );
+    reg a0,a1;
+    wire [3:0]D;
+two_four_decoder uut(a0,a1,D);
+initial
+begin
+a0=0;a1=0;
+#100 a0=1;a1=0;
+#100 a0=0;a1=1;
+#100 a0=1;a1=1;
+end
+endmodule
