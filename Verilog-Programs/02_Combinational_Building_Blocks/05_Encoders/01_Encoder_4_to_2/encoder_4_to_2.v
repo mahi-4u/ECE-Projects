d00a63bd9f10422e955000c7b6883122dafe843c
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-22 00:09:07 +0530

    Update encoder_4_to_2.v

--- Verilog-Programs/02_Combinational_Building_Blocks/05_Encoders/01_Encoder_4_to_2/encoder_4_to_2.v
+++ Verilog-Programs/02_Combinational_Building_Blocks/05_Encoders/01_Encoder_4_to_2/encoder_4_to_2.v
@@ -1,25 +1,33 @@
 //AUthor:mahesh
 //you can find three models of code
 module four_two_encoder(
-input a,b,c,d,
-output x,y
-    );
-    //GATE LEVEL MODELLING
-or(x,c,d);
-or(y,b,d);
+    input a, b, c, d,
+    output x, y
+);
+    wire a1, bbar;
 
-/*DATAFLOW MODELLING);
-assign x=c|d;
-assign y=b|d;*/
+    // GATE LEVEL MODELLING
+    or (x, a, b);
+    not (bbar, b);
+    and (a1, bbar, c);
+    or (y, a, a1);
 
-/*Behavioural
-always @(*)
-begin
-case({a,b,c,d})
-4'b1000:begin x=0;y=0; end
-4'b0100:begin x=0;y=1; end
-4'b0010:begin x=1;y=0; end
-4'b0001:begin x=1;y=1; end
-endcase
-end*/
+    /* DATAFLOW MODELLING
+    assign x = a | b;
+    assign y = a | (~b & c);
+    */
+
+    /* BEHAVIOURAL
+    reg x, y;
+    always @(*) begin
+        case ({a,b,c,d})
+            4'b1000: begin x=1; y=1; end
+            4'b0100: begin x=1; y=0; end
+            4'b0010: begin x=0; y=1; end
+            4'b0001: begin x=0; y=0; end
+            default: begin x=0; y=0; end
+        endcase
+    end
+    */
+
 endmodule
