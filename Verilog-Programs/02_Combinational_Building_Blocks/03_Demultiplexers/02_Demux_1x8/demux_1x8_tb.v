1dfd07e367b9af491e5c3b81ed26f3e59f16272c
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 22:10:29 +0530

    Update demux_1x8_tb.v

--- Verilog-Programs/02_Combinational_Building_Blocks/03_Demultiplexers/02_Demux_1x8/demux_1x8_tb.v
+++ Verilog-Programs/02_Combinational_Building_Blocks/03_Demultiplexers/02_Demux_1x8/demux_1x8_tb.v
@@ -1,1 +1,18 @@
+module one_eight_demuxtb(
+    );
+  reg d,s0,s1,s2;
+  wire [7:0]y;
+one_eight_demux uut(d,s0,s1,s2,y);
+initial
+begin
+d=1;s0=0;s1=0;s2=0;
+#100 s0=0;s1=0;s2=1;
+#100 s0=0;s1=1;s2=0;
+#100 s0=0;s1=1;s2=1;
+#100 s0=1;s1=0;s2=0;
+#100 s0=1;s1=0;s2=1;
+#100 s0=1;s1=1;s2=0;
+#100 s0=1;s1=1;s2=1;
 
+end
+endmodule
