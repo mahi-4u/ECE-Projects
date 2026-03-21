aee0727f6702b052655b86473ac5806ec1ce7189
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 21:58:06 +0530

    Update mux_4x1_tb.v

--- Verilog-Programs/02_Combinational_Building_Blocks/02_Multiplexers/01_Mux_4x1/mux_4x1_tb.v
+++ Verilog-Programs/02_Combinational_Building_Blocks/02_Multiplexers/01_Mux_4x1/mux_4x1_tb.v
@@ -1,1 +1,14 @@
-
+module four_one_mux_tb();
+reg [3:0]i;
+reg [1:0]s;
+wire out;
+four_one_mux uut(i,s,out);
+initial
+begin
+i[0]=1;i[1]=0;i[2]=0;i[3]=0;
+s[1]=0;s[0]=0;
+#100 s[1]=0;s[0]=1;
+#100 s[1]=1;s[0]=0;
+#100 s[1]=1;s[0]=1;
+end
+endmodule
