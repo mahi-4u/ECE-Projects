5c889390c9d46718b16b2c20ed7bb11d388eebb0
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 22:52:44 +0530

    Update binary_to_gray.v

--- Verilog-Programs/02_Combinational_Building_Blocks/01_code_converters/02_Binary_to_Gray/binary_to_gray.v
+++ Verilog-Programs/02_Combinational_Building_Blocks/01_code_converters/02_Binary_to_Gray/binary_to_gray.v
@@ -1,1 +1,29 @@
+//Author:mahesh
+//you can find three models of code
+module Bi_Grey_convert(
+output [3:0]G,
+input [3:0]B
+    );
+/*GATE LEVEL MODELLING
+buf(G[3],B[3]);
+xor A(G[2],B[3],B[2]);
+xor C(G[1],B[2],B[1]);
+xor D(G[0],B[1],B[0]);*/
 
+//DATAFLOW MODELLING
+assign G[3]=B[3];
+assign G[2]=B[3]^B[2];
+assign G[1]=B[2]^B[1];
+assign G[0]=B[1]^B[0];
+
+ /*BEHAVIOURAL MODELLING
+
+always @(*)
+begin
+G[3]=B[3];
+G[2]=B[3]^B[2];
+G[1]=B[2]^B[1];
+G[0]=B[1]^B[0];
+end
+*/
+endmodule
