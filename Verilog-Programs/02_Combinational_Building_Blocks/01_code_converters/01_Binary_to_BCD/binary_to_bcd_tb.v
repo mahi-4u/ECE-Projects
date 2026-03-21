aff7ba3012ce2ae7e68857de273a47558e83eca7
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 22:49:18 +0530

    Update binary_to_bcd_tb.v

--- Verilog-Programs/02_Combinational_Building_Blocks/01_code_converters/01_Binary_to_BCD/binary_to_bcd_tb.v
+++ Verilog-Programs/02_Combinational_Building_Blocks/01_code_converters/01_Binary_to_BCD/binary_to_bcd_tb.v
@@ -1,1 +1,16 @@
+module Bi_BCD_tb(
+    );
+reg [3:0]B;
+wire [4:0]R;
+Bi_to_BCD uut(.B(B),.R(R));
+initial
+begin
+B=4'b0000;
+#20 B=4'b0010;
+#20 B=4'b1100;
+#20 B=4'b1101;
+#20 B=4'b1110;
+#20 B=4'b1111;
+end
 
+endmodule
