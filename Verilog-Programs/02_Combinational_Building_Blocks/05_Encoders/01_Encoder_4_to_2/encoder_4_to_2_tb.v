002e180a786213842970c63a678cd0b2381e9e8d
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 22:45:34 +0530

    Update encoder_4_to_2_tb.v

--- Verilog-Programs/02_Combinational_Building_Blocks/05_Encoders/01_Encoder_4_to_2/encoder_4_to_2_tb.v
+++ Verilog-Programs/02_Combinational_Building_Blocks/05_Encoders/01_Encoder_4_to_2/encoder_4_to_2_tb.v
@@ -1,1 +1,12 @@
-
+module four_two_encodertb( );
+reg a,b,c,d;
+wire x,y;
+four_two_encoder uut(a,b,c,d,x,y);
+initial
+begin
+    a=1;b=0;c=0;d=0;
+#100a=0;b=1;c=0;d=0;
+#100a=0;b=0;c=1;d=0;
+#100a=0;b=0;c=0;d=1;
+end
+endmodule
