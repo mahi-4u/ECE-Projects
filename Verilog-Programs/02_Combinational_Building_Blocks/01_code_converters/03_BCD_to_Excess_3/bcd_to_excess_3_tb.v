7f730fa6eeea642d89916b80f8b454f7c5505e76
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 22:55:54 +0530

    Update bcd_to_excess_3_tb.v

--- Verilog-Programs/02_Combinational_Building_Blocks/01_code_converters/03_BCD_to_Excess_3/bcd_to_excess_3_tb.v
+++ Verilog-Programs/02_Combinational_Building_Blocks/01_code_converters/03_BCD_to_Excess_3/bcd_to_excess_3_tb.v
@@ -1,1 +1,16 @@
-
+module BCD_XS_tb(
+    );
+reg [3:0]B;
+wire [3:0]E;
+BCD_to_XS uut(B,E);
+initial
+begin
+B=4'b0000;
+#100 B=4'b0001;
+#100 B=4'b0010;
+#100 B=4'b0100;
+#100 B=4'b1000;
+#100 B=4'b1010;
+#100 B=4'b1111;
+end
+endmodule
