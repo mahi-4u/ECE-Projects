d40680c9a40b8c7ac0df24e3241be992347e410c
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 22:27:18 +0530

    Update demux_1x4_tb.v

--- Verilog-Programs/02_Combinational_Building_Blocks/03_Demultiplexers/01_Demux_1x4/demux_1x4_tb.v
+++ Verilog-Programs/02_Combinational_Building_Blocks/03_Demultiplexers/01_Demux_1x4/demux_1x4_tb.v
@@ -1,1 +1,14 @@
-
+module one_four_demuxtb(
+    );
+reg i,s1,s0;
+wire [3:0]y;
+one_four_demux uut(y,i,s1,s0);
+initial
+begin
+i=1;
+s1=0;s0=0;
+#50 s1=0;s0=1;
+#50 s1=1;s0=0;
+#50 s1=1;s0=1;
+end
+endmodule
