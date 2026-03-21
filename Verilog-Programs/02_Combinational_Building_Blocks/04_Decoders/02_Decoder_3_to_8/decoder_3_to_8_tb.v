fe4c39298b077cd7d33e5e9615463b67bb2e4041
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-21 22:38:30 +0530

    Update decoder_3_to_8_tb.v

--- Verilog-Programs/02_Combinational_Building_Blocks/04_Decoders/02_Decoder_3_to_8/decoder_3_to_8_tb.v
+++ Verilog-Programs/02_Combinational_Building_Blocks/04_Decoders/02_Decoder_3_to_8/decoder_3_to_8_tb.v
@@ -1,1 +1,16 @@
-
+module three_eight_decodertb();
+reg a0,a1,a2;
+wire [7:0]D;
+three_eight_decoder uut(a0,a1,a2,D);
+initial
+begin
+a0=0;a1=0;a2=0;
+#100 a0=0;a1=0;a2=1;
+#100 a0=0;a1=1;a2=0;
+#100 a0=0;a1=1;a2=1;
+#100 a0=1;a1=0;a2=0;
+#100 a0=1;a1=0;a2=1;
+#100 a0=1;a1=1;a2=0;
+#100 a0=1;a1=1;a2=1;
+end 
+endmodule
