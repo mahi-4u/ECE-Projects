1e09d34da3d0770e7dc37bdf3940721b46cb9811
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-30 22:32:01 +0530

    Update arduino-code.ino

--- IoT-Projects/03_Mini_Projects/06_line-follower-robot/arduino-code.ino
+++ IoT-Projects/03_Mini_Projects/06_line-follower-robot/arduino-code.ino
@@ -1,1 +1,98 @@
+//Author:mahesh
+#define IR_SENSOR_RIGHT 11
+#define IR_SENSOR_LEFT 12
+#define MOTOR_SPEED 150
+// Right motor
+int enableRightMotor = 6;
+int rightMotorPin1 = 7;
+int rightMotorPin2 = 8;
+// Left motor
+int enableLeftMotor = 5;
+int leftMotorPin1 = 9;
+int leftMotorPin2 = 10;
+// Function declaration
+void rotateMotor(int rightMotorSpeed, int leftMotorSpeed);
+void setup()
+{
+  // Optional: Change PWM frequency for smoother motor control
+  TCCR0B = (TCCR0B & B11111000) | B00000010;
+  // Motor pins
+  pinMode(enableRightMotor, OUTPUT);
+  pinMode(rightMotorPin1, OUTPUT);
+  pinMode(rightMotorPin2, OUTPUT);
+  pinMode(enableLeftMotor, OUTPUT);
+  pinMode(leftMotorPin1, OUTPUT);
+  pinMode(leftMotorPin2, OUTPUT);
+  // IR sensors
+  pinMode(IR_SENSOR_RIGHT, INPUT);
+  pinMode(IR_SENSOR_LEFT, INPUT);
+  // Stop motors initially
+  rotateMotor(0, 0);
+}
+void loop()
+{
+  int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
+  int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
+  // Both sensors on white → Move straight
+  if (rightIRSensorValue == LOW && leftIRSensorValue == LOW)
+  {
+    rotateMotor(MOTOR_SPEED, MOTOR_SPEED);
+  }
+  // Right sensor on black → Turn right
+  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW)
+  {
+    rotateMotor(-MOTOR_SPEED, MOTOR_SPEED);
+  }
+  // Left sensor on black → Turn left
+  else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH)
+  {
+    rotateMotor(MOTOR_SPEED, -MOTOR_SPEED);
+  }
+
+  // Both sensors on black → Stop
+  else
+  {
+    rotateMotor(0, 0);
+  }
+}
+// Function to control motors
+void rotateMotor(int rightMotorSpeed, int leftMotorSpeed)
+{
+  // Right motor control
+  if (rightMotorSpeed < 0)
+  {
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, HIGH);
+  }
+  else if (rightMotorSpeed > 0)
+  {
+    digitalWrite(rightMotorPin1, HIGH);
+    digitalWrite(rightMotorPin2, LOW);
+  }
+  else
+  {
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, LOW);
+  }
+
+  // Left motor control
+  if (leftMotorSpeed < 0)
+  {
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, HIGH);
+  }
+  else if (leftMotorSpeed > 0)
+  {
+    digitalWrite(leftMotorPin1, HIGH);
+    digitalWrite(leftMotorPin2, LOW);
+  }
+  else
+  {
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, LOW);
+  }
+  // Apply speed using PWM
+  analogWrite(enableRightMotor, abs(rightMotorSpeed));
+  analogWrite(enableLeftMotor, abs(leftMotorSpeed));
+}
 
