9fd4a01be3985323f918e4567ebcb4367f24cb42
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-30 21:23:22 +0530

    Update radar_system.ino

--- IoT-Projects/03_Mini_Projects/01_Radar_System(basic)/radar_system.ino
+++ IoT-Projects/03_Mini_Projects/01_Radar_System(basic)/radar_system.ino
@@ -1,1 +1,50 @@
+//Author:Mahesh
 
+#include<Servo.h>
+#define trigPin 9
+#define echoPin 10
+long duration;
+int distance ;
+Servo myservo;
+int calculateDistance()
+{
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration*0.034/2;
+  return distance;
+}
+void setup()
+{
+  pinMode(trigPin , OUTPUT);
+  pinMode(echoPin, INPUT);
+  myservo.attach(11);
+  Serial.begin(9600);
+}
+void loop()
+{
+ int i ;
+ for (i=15; i<=165; i++)
+ {
+  myservo.write(i);
+  delay(15);
+  calculateDistance();
+  Serial.println("angle:");
+  Serial.println(i);
+  Serial.println("distance:");
+  Serial.println(distance); 
+ }
+ for(i=165; i>=15; i--)
+ {
+  myservo.write(i);
+  delay(15);
+  calculateDistance();
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print(".");
+ }
+}
