8d7f5e340427caf3eef91b3c3a60d952e47165d6
Mahi's-electronics <ismahi4u@gmail.com>
2026-03-30 21:44:30 +0530

    Update door-lock.ino

--- IoT-Projects/03_Mini_Projects/02_Door_lock_system/door-lock.ino
+++ IoT-Projects/03_Mini_Projects/02_Door_lock_system/door-lock.ino
@@ -1,1 +1,86 @@
+//Author:mahesh
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+#include <Servo.h>
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+Servo servoLock;
+
+const byte ROWS = 4;
+const byte COLS = 3;
+
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+String password = "1234";
+String inputPassword = "";
+
+void setup() {
+  lcd.begin(16, 2);     // FIXED
+  lcd.backlight();
+
+  servoLock.attach(10);
+  servoLock.write(0);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Door Locked");
+  delay(1500);
+  lcd.clear();
+}
+
+void loop() {
+  lcd.setCursor(0, 0);
+  lcd.print("Enter Password:");
+
+  char key = keypad.getKey();
+
+  if (key) {
+    if (key == '#') {
+      if (inputPassword == password) {
+        unlockDoor();
+      } else {
+        wrongPass();
+      }
+      inputPassword = "";
+    } 
+    else if (key == '*') {
+      inputPassword = "";
+      lcd.clear();
+    } 
+    else {
+      inputPassword += key;
+      lcd.setCursor(0, 1);
+      lcd.print(inputPassword);
+    }
+  }
+}
+
+void unlockDoor() {
+  lcd.clear();
+  lcd.print("Access Granted");
+  servoLock.write(90);
+  delay(2000);
+  lockDoor();
+}
+
+void wrongPass() {
+  lcd.clear();
+  lcd.print("Wrong Password");
+  delay(1500);
+  lcd.clear();
+}
+
+void lockDoor() {
+  lcd.clear();
+  lcd.print("Door Locked");
+  servoLock.write(0);
+}
