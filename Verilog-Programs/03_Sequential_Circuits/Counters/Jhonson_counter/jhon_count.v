9dd57c5668892809fde4ae432214a8918ea9bf9e
Mahi's-electronics <ismahi4u@gmail.com>
2026-04-30 19:41:13 +0530

    Create jhon_count.v

---
+++ Verilog-Programs/03_Sequential_Circuits/Counters/Jhonson_counter/jhon_count.v
@@ -0,0 +1,21 @@
+//Author:mahesh
+//here is my code for the jhonson counter
+module johnson_counter(
+    input clk, clr,
+    output reg [3:0] q
+);
+
+always @(posedge clk or posedge clr)
+begin
+    if (clr)
+        q <= 4'b0000;
+    else
+    begin
+        q[3] <= ~q[0];
+        q[2] <= q[3];
+        q[1] <= q[2];
+        q[0] <= q[1];
+    end
+end
+
+endmodule
