c0f2dc6ba1dc5118152efd830a2e7a4f6faa8b5f
Mahi's-electronics <ismahi4u@gmail.com>
2026-05-19 00:15:09 +0530

    Create set_operations.py

---
+++ python-codes/06_Datatypes.py/03_sets.py/set_operations.py
@@ -0,0 +1,12 @@
+# Program to perform basic set operations
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+print("Set A:", A)
+print("Set B:", B)
+
+print("Union:", A.union(B))
+print("Intersection:", A.intersection(B))
+print("Difference (A - B):", A.difference(B))
+print("Symmetric Difference:", A.symmetric_difference(B))
