6bfaaaa02c3d35e7f50a664897dc8b01714d38ec
Mahi's-electronics <ismahi4u@gmail.com>
2026-05-19 00:11:45 +0530

    Create count_element.py

---
+++ python-codes/06_Datatypes.py/02_Tuple.py/count_element.py
@@ -0,0 +1,9 @@
+# Program to count how many times an element appears in a tuple
+
+numbers = (10, 20, 30, 20, 40, 20, 50)
+
+element = 20
+count = numbers.count(element)
+
+print("Tuple:", numbers)
+print(f"{element} appears {count} times.")
